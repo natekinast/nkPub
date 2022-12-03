05fce588bc1b2bc20e30fcbe9126677379ebd73a
natekinast <101215078+natekinast@users.noreply.github.com>
2022-12-02 20:54:16 -0600

    Create riddlerWorldCup.py

---
+++ riddlerWorldCup.py
@@ -0,0 +1,26 @@
+from random import randint
+trials = 10000000
+busResult = 0
+lastUSResult = 0
+
+for i in range(0, trials):
+    usFans = 11
+    nedFans = 7
+    buses = 0
+    lastPick = 2
+
+    while usFans != 0 and nedFans != 0:
+        if randint(0, 1):
+            buses += 0 if lastPick == 1 else 1
+            usFans -= 1
+            lastPick = 1
+        else:
+            buses += 0 if lastPick == 0 else 1
+            nedFans -= 1
+            lastPick = 0
+
+    lastUSResult += 0 if usFans == 0 else 1
+    busResult += buses
+
+print(f'Probability last bus was US fans: {lastUSResult/trials}')
+print(f'Average number of buses: {busResult/trials}')
